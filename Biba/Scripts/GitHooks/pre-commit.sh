49f657fee42c778af2e1f0701ac3fb849d5c7911
Andy Wu <andywu.england@gmail.com>
2023-12-21 10:33:00 +0800

    2

--- Biba/Scripts/GitHooks/pre-commit.sh
+++ Biba/Scripts/GitHooks/pre-commit.sh
@@ -12,3 +12,7 @@
 # pass information which files to be excluded to git-format-stages as --exclude option in .swiftformat config file is not honored by git-format-staged
 #git-format-staged --formatter "swiftformat stdin --stdinpath '{}'" "*.swift"
 
+git diff --diff-filter=d --staged --name-only | grep -e '\(.*\).swift$' | while read line; do
+    swift-format -m format -i "${line}";
+    git add "$line";
+done

b53b14ce53d5359c94ce02e860c60ca8e6fe6082
Andy Wu <andywu.england@gmail.com>
2023-12-24 23:57:27 +0800

    fixed

--- start.rb
+++ start.rb
@@ -10,7 +10,7 @@ project_path = 'Biba.xcodeproj'
 #open the xcode project
 project = Xcodeproj::Project.open(project_path)
 #find the group on which you want to add the file
-group = project.main_group["Biba"]["App"]["Config"]
+group = project.main_group["Biba"]["App"]
 #get the file reference for the file to add
 file = group.new_file("Config.swift")
 #add the file reference to the projects first target
